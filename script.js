b0883f3e63c366c8c06ac6e8f69cc0524b164ea9
Esm3il <esmilgamer97@gmail.com>
2023-09-16 17:34:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,39 +22,39 @@ function submit(){
     //Checking Inputs
     if((name.value.length == 0)){
         name.style.backgroundColor = "#ff5252";
-        noti.innerHTML = "Input Error try to put gpa from 1 to 5 only";
+        noti.innerHTML = "Input Error in name";
         status = 1;
     }
-    if((year1.value > 5)||(year1.value < 0)||(year1.value.length == 0)){
+    if((year1.value > 5 || year1.value < 1)||(year1.value < 0)||(year1.value.length == 0)){
         year1.style.backgroundColor = "#ff5252";
-        noti.innerHTML = "Input Error try to put gpa from 1 to 5 only";
+        noti.innerHTML = "Input Error in year1";
         status = 1;
     }
-    if((year2.value > 5)||(year2.value < 0)||(year2.value.length == 0)){
+    if((year2.value > 5 || year2.value < 1)||(year2.value < 0)||(year2.value.length == 0)){
         year2.style.backgroundColor = "#ff5252";
-        noti.innerHTML = "Input Error try to put gpa from 1 to 5 only";
+        noti.innerHTML = "Input Error in year2";
         status = 1;
     }
-    if((year3.value > 5)||(year3.value < 0)||(year3.value.length == 0)){
+    if((year3.value > 5 || year3.value < 1)||(year3.value < 0)||(year3.value.length == 0)){
         year3.style.backgroundColor = "#ff5252";
-        noti.innerHTML = "Input Error try to put gpa from 1 to 5 only";
+        noti.innerHTML = "Input Error in year3";
         status = 1;
     }
-    if((year4.value > 5)||(year4.value < 0)||(year4.value.length == 0)){
+    if((year4.value > 5 || year4.value < 1)||(year4.value < 0)||(year4.value.length == 0)){
         year4.style.backgroundColor = "#ff5252";
-        noti.innerHTML = "Input Error try to put gpa from 1 to 5 only";
+        noti.innerHTML = "Input Error in year4";
         status = 1;
     }
-    if((year5.value > 5)||(year5.value < 0)||(year5.value.length == 0)){
+    if((year5.value > 5 || year5.value < 1)||(year5.value < 0)||(year5.value.length == 0)){
         year5.style.backgroundColor = "#ff5252";
-        noti.innerHTML = "Input Error try to put gpa from 1 to 5 only";
+        noti.innerHTML = "Input Error in year5";
         status = 1;
     }
 
     //calculating GPA
     if(status == 1){return 0;}
     else{
-        GPA = (parseFloat(year1.value) + parseFloat(year2.value) + parseFloat(year3.value) + parseFloat(year4.value) + parseFloat(year5.value))/5;
+        GPA = ((parseFloat(year1.value) + parseFloat(year2.value) + parseFloat(year3.value) + parseFloat(year4.value) + parseFloat(year5.value))/5).toFixed(2);
         noti.innerHTML = `${name.value} your GPA is ${GPA}`;
     }
 }
